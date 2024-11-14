48e7a938b3119885e3843a77e44eebeaefaf5d97
Ezeh Godwin <ezehgodwin3942@gmail.com>
2024-11-14 04:26:02 +0100

    worked on flight pages

--- script/routes.js
+++ script/routes.js
@@ -2,6 +2,8 @@
     let homes = document.querySelectorAll("#home");
     let regionLanguages = document.querySelectorAll("#region-language");
     let currencys = document.querySelectorAll("#currency");
+    let regionLanguagesInner = document.querySelectorAll("#region-language-inner");
+    let currencysInner = document.querySelectorAll("#currency-inner");
 
     // mationality and currency section stert
     let linkStyle1 = document.getElementById("link-style-1");
@@ -15,6 +17,15 @@
     let hotelLink = document.querySelectorAll("#hotel-link");
     let clubLink = document.querySelectorAll("#club-link");
     let sportLink = document.querySelectorAll("#sport-link");
+
+    // flight inner links 
+    let oneWay = document.querySelectorAll("#one-way");
+    let roundTrip = document.querySelectorAll("#round-trip");
+    let multiCity = document.querySelectorAll("#multi-city");
+
+    // flight link styles
+    let flightLink1 = document.getElementById("flight-link1")
+    let flightLink2 = document.getElementById("flight-link2")
     
     
     homes.forEach(home => {
@@ -37,8 +48,43 @@
             linkStyle2.classList.add("active")
         } 
     })
+
+    // regional language an dcurrency inner link
+    regionLanguagesInner.forEach(regionLanguage => {
+        regionLanguage.href = "region-language.html";
+        if (currentPath.includes(regionLanguage.getAttribute("href"))) {
+            linkStyle1.classList.add("active")
+        }
+    });
+
+    currencysInner.forEach(currency => {
+        currency.href = "currency.html";
+        if (currentPath.includes(currency.getAttribute("href"))) {
+            linkStyle2.classList.add("active");
+        } ;
+    });
 // regional language and currency links ends here
 
 flightLink.forEach(flight => {
     flight.href = "html/flight.html";
+})
+
+
+/* ---------- flight inner link starts here -------*/
+oneWay.forEach(link => {
+    link.href = "flight.html";
+    if (currentPath.includes(link.getAttribute("href"))) {
+        flightLink1.classList.add("flight-active");
+        flightLink1.style.backgroundColor = "brown";
+        link.style.color = "white";
+    } ;
+})
+
+roundTrip.forEach(link => {
+    link.href = "roundTrip.html";
+    if (currentPath.includes(link.getAttribute("href"))) {
+        flightLink2.classList.add("flight-active");
+        flightLink2.style.backgroundColor = "brown";
+        link.style.color = "white";
+    } ;
 })
